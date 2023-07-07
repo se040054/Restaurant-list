91d8c345414fdca906569966647ed1f273a42626
se040054 <peter8822967@gmail.com>
2023-07-07 18:08:16 +0800

    feat : modify search function , now can search just name and category of restaurants

--- app.js
+++ app.js
@@ -18,16 +18,9 @@ app.get('/', (req, res) => {
 app.get('/Restaurants',(req,res)=>{
   const keyword = req.query.search?.trim() ;
   const matchedRestaurant = keyword ? 
-  restaurants.filter(restaurant => 
-    Object.values(restaurant).some(property => {
-      if (typeof property === 'string') {
-            return property.toLowerCase().includes(keyword.toLowerCase())  
-    }
-            return false
-    })
-  )
+  restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword))
   : restaurants ;
-  console.log(keyword)
+  
   res.render('index',{restaurants:matchedRestaurant,keyword})
 })
 
